f471f61cebd59b8ad501c67db44c33c72ced9ae4
Prakash Jbb <prakashmeghwaljbb0521@gmail.com>
2023-04-13 16:51:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello world")
+
+
+// function plusMinus(){
+    
+//     let value = true;
+    
+//     if(value==false){
+//         display.value += '-';
+//         let value = true;        
+//     }
+//     else if(value==true){        
+//         display.value += '+';
+//         let value = false;
+//     }
+
+
+// }
+
+// M+/- Button
+// No repeat Operator and .
+// Input max length
+
+// document.querySelectorAll('input[type="number"]').forEach(input => {
+//     input.oninput = () =>{
+//     if (input.value.length > input.maxLength){
+        
+//      input.value = input.value.slice(0,maxlength); 
+//     }
+//     console.log("A");
+        
+//     }
+// })
